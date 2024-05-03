329527bf625a2f2cad6857c5b3c3ff691fd4663a
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-03 18:00:38 -0300

    feat: new layout
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- authentication/app.js
+++ authentication/app.js
@@ -1,12 +1,35 @@
 const buttonLogin = document.getElementById('buttonLogin')
-const imputEmail = document.getElementById('emailInput')
-const imputPassoword = document.getElementById('passwordInput')
+const imputEmail = document.getElementById('UserEmail')
+const imputPassoword = document.getElementById('UserPassoword')
 
-buttonLogin.addEventListener('click', (event) => {
-    event.preventDefault()
+buttonLogin.addEventListener('click', () => {
 
     const emailUser = imputEmail.value
     const passwordUser = imputPassoword.value
 
-    console.log(emailUser, passwordUser)
+    let listaUser = []
+    let userValid = {
+        nome: '',
+        email: '',
+        senha: ''
+    }
+    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+
+    listaUser.forEach((item) => {
+        if (emailUser == item.emailCAD && passwordUser == item.senhaCAD) {
+            
+            userValid = {
+                nome: item.nameCAD,
+                email: item.emailCAD,
+                senha: item.senhaCAD
+            }
+        }
+    })
+ if(emailUser == userValid.email && passwordUser == userValid.senha){
+    let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2) 
+    localStorage.setItem('token', token)
+    window.location.href ='http://127.0.0.1:5500/page/Status.html'
+ }else {
+    alert('deu ruim')
+ }
 })
--- authentication/registro.js
+++ authentication/registro.js
@@ -33,6 +33,7 @@ CadastrarButtom.addEventListener('click', () => {
         )
         localStorage.setItem('listaUser', JSON.stringify(listaUser))
         clear()
+        window.location.href = 'http://127.0.0.1:5500/authentication/SignIn.html'
     }
 })
 
