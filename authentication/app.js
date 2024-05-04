087fe8095924e4781cde5b7e36475ca7ff9b3ab3
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-03 21:19:37 -0300

    feat: deploy
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- authentication/app.js
+++ authentication/app.js
@@ -28,6 +28,7 @@ buttonLogin.addEventListener('click', () => {
  if(emailUser == userValid.email && passwordUser == userValid.senha){
     let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2) 
     localStorage.setItem('token', token)
+    localStorage.setItem('userLogado',JSON.stringify(userValid))
     window.location.href ='http://127.0.0.1:5500/page/Status.html'
  }else {
     alert('deu ruim')
